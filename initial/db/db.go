22546fc0ac204011959943f583c33f5036784407
Nahid Ghorbai <n.ghorbani@graph-inc.ir>
2025-10-21 10:57:27 +0330

    formating file

--- initial/db/db.go
+++ initial/db/db.go
@@ -2,11 +2,11 @@ package db
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 func Connect() *gorm.DB {
@@ -15,17 +15,17 @@ func Connect() *gorm.DB {
 	}
 
 	host := os.Getenv("DB_HOST")
-	user:= os.Getenv("DB_USER")
+	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf(
-		"host=%v user=%v password=%v dbname=%v port=%v sslmode=disable", 
+		"host=%v user=%v password=%v dbname=%v port=%v sslmode=disable",
 		host, user, password, dbname, port,
 	)
 
-	db , err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
